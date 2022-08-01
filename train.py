d7427d8c5135a350d61c8eab0385b3ae9749a271
BBuf <1182563586@qq.com>
2022-08-01 08:31:04 +0000

    fix non_contiguous bug

--- train.py
+++ train.py
@@ -115,17 +115,17 @@ def train(hyp,  # path/to/hyp.yaml or hyp dictionary
     is_coco = isinstance(val_path, str) and val_path.endswith('coco/val2017.txt')  # COCO dataset
 
     # Model
-    check_suffix(weights, '.pt')  # check weights
+    # check_suffix(weights, '.pt')  # check weights
 #    pretrained = weights.endswith('.pt')
     pretrained = False
     if pretrained:
         # with torch_distributed_zero_first(LOCAL_RANK):
         #     weights = attempt_download(weights)  # download if not found locally
-        ckpt = flow.load('ckpt')  # load checkpoint
+        ckpt = flow.load(weights, map_location='cpu')  # load checkpoint to CPU to avoid CUDA memory leak
         model = Model(cfg or ckpt['model'].yaml, ch=3, nc=nc, anchors=hyp.get('anchors')).to(device)  # create
-    #     exclude = ['anchor'] if (cfg or hyp.get('anchors')) and not resume else []  # exclude keys
-        csd = ckpt['model']  # checkpoint state_dict as FP32
-    #     csd = intersect_dicts(csd, model.state_dict(), exclude=exclude)  # intersect
+        exclude = ['anchor'] if (cfg or hyp.get('anchors')) and not resume else []  # exclude keys
+        csd = ckpt['model'].float().state_dict()  # checkpoint state_dict as FP32
+        csd = intersect_dicts(csd, model.state_dict(), exclude=exclude)  # intersect
         model.load_state_dict(csd, strict=False)  # load
         LOGGER.info(f'Transferred {len(csd)}/{len(model.state_dict())} items from {weights}')  # report
     else:
