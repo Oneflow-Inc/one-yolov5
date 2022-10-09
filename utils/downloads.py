aa62098edd7c9bd26cc96f2b2ec3ca2f5199b76f
ccsuu <ccsuwen@gmail.com>
2022-10-09 10:30:50 +0000

    fix_run_settings

--- utils/downloads.py
+++ utils/downloads.py
@@ -16,6 +16,16 @@ from zipfile import ZipFile
 import requests
 from tqdm import tqdm
 
+def is_url(url, check=True):
+    # Check if string is URL and check if URL exists
+    try:
+        url = str(url)
+        result = urllib.parse.urlparse(url)
+        assert all([result.scheme, result.netloc])  # check if is url
+        return (urllib.request.urlopen(url).getcode() == 200) if check else True  # check if exists online
+    except (AssertionError, urllib.request.HTTPError):
+        return False
+
 
 def gsutil_getsize(url=""):
     # gs://bucket/file size https://cloud.google.com/storage/docs/gsutil/commands/du
