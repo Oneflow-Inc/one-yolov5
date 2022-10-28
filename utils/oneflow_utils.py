7e149919d6245421c18bb494a73d15fa27094337
ccsuu <ccsuwen@gmail.com>
2022-10-28 12:52:22 +0000

    Update oneflow_utils.py

--- utils/oneflow_utils.py
+++ utils/oneflow_utils.py
@@ -88,8 +88,9 @@ def select_device(device="", batch_size=0, newline=True):
 
 def time_sync():
     # oneflow-accurate time
-    if flow.cuda.is_available():
-        flow.cuda.synchronize()
+    # TODO(fengwen):在ddp训练计算loss时候卡死,暂时注释下面两行，先绕过
+    # if flow.cuda.is_available():
+    #     flow.cuda.synchronize()
     return time.time()
 
 
