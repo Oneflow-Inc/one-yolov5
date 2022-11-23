8947520314871ea129fc87ccce193ada10206f8b
BBuf <1182563586@qq.com>
2022-11-23 07:29:01 +0000

    change api

--- utils/metrics.py
+++ utils/metrics.py
@@ -259,7 +259,7 @@ def bbox_iou(box1, box2, xywh=True, GIoU=False, DIoU=False, CIoU=False, bbox_iou
             if CIoU:  # https://github.com/Zzh-tju/DIoU-SSD-pyflow/blob/master/utils/box/box_utils.py#L47
                 v = (4 / math.pi ** 2) * flow.pow(flow.atan(w2 / (h2 + eps)) - flow.atan(w1 / (h1 + eps)), 2)
                 if bbox_iou_optim:
-                    return flow._C.fused_get_ciou_result(v, iou, rho2, c2, eps)
+                    return flow._C.fused_get_ciou_result(v, iou, rho2, c2, eps)[0]
                 else:
                     with flow.no_grad():
                         alpha = v / (v - iou + (1 + eps))
