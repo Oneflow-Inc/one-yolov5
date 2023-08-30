ad30edc43cd90b7ad2187ee1e83fd8de200a2900
ShawnXuan <xiexuanx2@gmail.com>
2023-08-30 13:52:03 +0800

    ok

---
+++ run.sh
@@ -0,0 +1,7 @@
+python segment/train.py \
+        --weights /workspace/data/yolov5s-seg.pt \
+        --data data/coco128-seg.yaml \
+        --project checkpoint \
+        --name exp \
+        --epochs 2 \
+        --img 640
--- utils/general.py
+++ utils/general.py
@@ -356,7 +356,10 @@ def check_git_info(path="."):
     try:
         repo = git.Repo(path)
         remote = repo.remotes.origin.url.replace(".git", "")  # i.e. 'https://github.com/ultralytics/yolov5'
-        commit = repo.head.commit.hexsha  # i.e. '3134699c73af83aac2a481435550b968d5792c0d'
+        try:
+            commit = repo.head.commit.hexsha  # i.e. '3134699c73af83aac2a481435550b968d5792c0d'
+        except ValueError:
+            commit = None
         try:
             branch = repo.active_branch.name  # i.e. 'main'
         except TypeError:  # not on any branch
