3a67c59f17c771c9b53a3dd6d4420759b8ffb340
ShawnXuan <xiexuanx2@gmail.com>
2023-08-30 16:02:46 +0800

    add dockerfile

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 python segment/train.py \
-        --weights /workspace/data/yolov5s-seg.pt \
+        --weights /workspace/data/yolov5x-seg.pt \
         --data data/coco128-seg.yaml \
         --project checkpoint \
         --name exp \
-        --epochs 2 \
+        --epochs 5 \
         --img 640
