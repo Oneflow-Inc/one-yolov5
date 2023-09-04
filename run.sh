deb9e5fce0cabff0bcb3d34e0c44028952bb3b39
ShawnXuan <xiexuanx2@gmail.com>
2023-09-04 20:27:31 +0800

    update

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 python segment/train.py \
-        --weights /workspace/data/yolov5x-seg.pt \
+        --weights /code/data/yolov5x-seg.pt \
         --data data/coco128-seg.yaml \
         --project checkpoint \
         --name exp \
