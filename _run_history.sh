02c9003843cb13b579e6cc7a35c3e2398372be61
ccsuu <ccsuwen@gmail.com>
2023-10-25 05:51:01 +0000

    refine

---
+++ _run_history.sh
@@ -0,0 +1,10 @@
+python train.py --weights yolov5s.of --device 0 
+
+
+python train.py --resume /home/fengwen/YOLOV5/one-yolov5/runs/train/exp/weights/last.of
+
+
+python val.py --weights yolov5x.of --data data/coco128.yaml --img 640 --half
+
+
+python detect.py --weights yolov5s.of --img 832 --source data/images/ --conf-thres 0.8
